7f96d21f2b81b15193a3b940601c8dd735a992f0
Matteo Leonelli <matteo@MacBook-Pro-di-Matteo.local>
2020-04-16 03:37:12 +0200

    log header e body request + riorganizzazione codice

--- PullprintingMobileApp/app/src/main/java/com/example/myapplication/AppAuthGlideModule.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public final class AppAuthGlideModule extends AppGlideModule {
-
-}
